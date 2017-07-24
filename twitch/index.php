d2be7de9dd79ea02df5fb4e3c1b2e25c6860ea76
eldrinduran <eldrin.duran92@gmail.com>
2017-07-24 21:29:43 +0800

    added script tags

--- twitch/index.php
+++ twitch/index.php
@@ -1,9 +1,32 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Twitch</title>
+	<title>Twitch Streamers	</title>
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+
 </head>
 <body>
+<nav class="navbar navbar-default">
+	<div class="container">
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+      	</button>
+	</div>
+     <a class="navbar-brand" href="#">TWITCH STREAMERS</a>
+	</div>
+</nav>
 
+<div class="container">
+	<h1>Hello World</h1>	
+</div>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="scripts/script.js"></script>
+	
 </body>
 </html>
